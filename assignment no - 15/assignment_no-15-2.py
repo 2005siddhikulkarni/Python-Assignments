068596cdf01f060e4d8754f4a2ce7ef06d39d109
2005siddhikulkarni <siddhikulkarni200@gmail.com>
2026-01-27 01:09:48 +0530

    Update assignment_no-15-2.py

--- assignment no - 15/assignment_no-15-2.py
+++ assignment no - 15/assignment_no-15-2.py
@@ -2,8 +2,16 @@
 EvenNo = lambda No: No % 2 == 0
 
 def main():
-    lst = list(map(int, input("Enter numbers: ").split()))
+    lst = []
 
+    print("Enter the number: ")
+    No = int(input())
+
+    print("Enter the numbers: ")
+    for i in range(1,No+1):
+        Values = int(input())
+        lst.append(Values)
+        
     Result = list(filter(EvenNo, lst))
     print("Even numbers are: ",Result)
 
