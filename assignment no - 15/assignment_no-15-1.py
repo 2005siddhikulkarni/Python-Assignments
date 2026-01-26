c4fc40c8d88b0813a8037d3a2ab7431f587dc795
2005siddhikulkarni <siddhikulkarni200@gmail.com>
2026-01-27 01:08:59 +0530

    Update assignment_no-15-1.py

--- assignment no - 15/assignment_no-15-1.py
+++ assignment no - 15/assignment_no-15-1.py
@@ -2,11 +2,18 @@
 Square = lambda Nos : Nos * Nos
 
 def main():
-    lst = list(map(int, input("Enter numbers: ").split()))
+    lst = []
+
+    print("Enter the number: ")
+    No = int(input())
+
+    print("Enter the numbers: ")
+    for i in range(1,No+1):
+        Values = int(input())
+        lst.append(Values)
 
     Result = list(map(Square, lst))
     print(Result)
-
 
 if __name__ == "__main__":
     main()
