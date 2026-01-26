39401d02369c717b7426b1af2e3b4ebd7a2cfeca
2005siddhikulkarni <siddhikulkarni200@gmail.com>
2026-01-27 01:11:37 +0530

    Update assignment_no-15-5.py

--- assignment no - 15/assignment_no-15-5.py
+++ assignment no - 15/assignment_no-15-5.py
@@ -3,7 +3,15 @@ from functools import reduce
 MaxNo = lambda No1,No2 : No1 if No1 > No2 else No2
 
 def main():
-    lst = list(map(int, input("Enter numbers: ").split()))
+    lst = []
+
+    print("Enter the number: ")
+    No = int(input())
+
+    print("Enter the numbers: ")
+    for i in range(1, No + 1):
+        Values = int(input())
+        lst.append(Values)
 
     Result = reduce(MaxNo, lst)
     print("Maximum number is: ",Result)
