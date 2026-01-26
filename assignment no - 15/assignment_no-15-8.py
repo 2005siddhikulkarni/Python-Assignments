ebfc78365980fe1b87d17a6f860525771ebc3243
2005siddhikulkarni <siddhikulkarni200@gmail.com>
2026-01-27 01:13:11 +0530

    Update assignment_no-15-8.py

--- assignment no - 15/assignment_no-15-8.py
+++ assignment no - 15/assignment_no-15-8.py
@@ -2,7 +2,15 @@
 Divisible = lambda No : No % 3 == 0 and No % 5 == 0
 
 def main():
-    lst = list(map(int, input("Enter numbers: ").split()))
+    lst = []
+
+    print("Enter the number: ")
+    No = int(input())
+
+    print("Enter the numbers: ")
+    for i in range(1, No + 1):
+        Values = int(input())
+        lst.append(Values)
 
     Result = list(map(Divisible, lst))
     print(Result)
