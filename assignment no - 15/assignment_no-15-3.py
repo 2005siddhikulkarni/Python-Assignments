8ad3c27e401f212fd50720ad1d3b531a7e4764ac
2005siddhikulkarni <siddhikulkarni200@gmail.com>
2026-01-27 01:10:21 +0530

    Update assignment_no-15-3.py

--- assignment no - 15/assignment_no-15-3.py
+++ assignment no - 15/assignment_no-15-3.py
@@ -2,7 +2,15 @@
 OddNo = lambda No: No % 2 != 0
 
 def main():
-    lst = list(map(int, input("Enter numbers: ").split()))
+    lst = []
+
+    print("Enter the number: ")
+    No = int(input())
+
+    print("Enter the numbers: ")
+    for i in range(1,No+1):
+        Values = int(input())
+        lst.append(Values)
 
     Result = list(filter(OddNo, lst))
     print("Odd numbers are: ",Result)
