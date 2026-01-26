12f8a849bf973d5a8ecbbdaa50e9cf9f750bc5ea
2005siddhikulkarni <siddhikulkarni200@gmail.com>
2026-01-27 01:12:07 +0530

    Update assignment_no-15-6.py

--- assignment no - 15/assignment_no-15-6.py
+++ assignment no - 15/assignment_no-15-6.py
@@ -3,8 +3,16 @@ from functools import reduce
 MinNo = lambda No1,No2 : No1 if No1 < No2 else No2
 
 def main():
-    lst = list(map(int, input("Enter numbers: ").split()))
+    lst = []
 
+    print("Enter the number: ")
+    No = int(input())
+
+    print("Enter the numbers: ")
+    for i in range(1, No + 1):
+        Values = int(input())
+        lst.append(Values)
+        
     Result = reduce(MinNo, lst)
     print("Minimum number is: ",Result)
 
