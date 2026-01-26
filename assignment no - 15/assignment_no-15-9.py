2c53374f049aeddae076e234b9f9f7ab9cbbe6e8
2005siddhikulkarni <siddhikulkarni200@gmail.com>
2026-01-27 01:13:42 +0530

    Update assignment_no-15-9.py

--- assignment no - 15/assignment_no-15-9.py
+++ assignment no - 15/assignment_no-15-9.py
@@ -3,7 +3,15 @@ from functools import reduce
 Multiply = lambda No1,No2 : No1 * No2 
 
 def main():
-    lst = list(map(int, input("Enter numbers: ").split()))
+    lst = []
+
+    print("Enter the number: ")
+    No = int(input())
+
+    print("Enter the numbers: ")
+    for i in range(1, No + 1):
+        Values = int(input())
+        lst.append(Values)
 
     Result = reduce(Multiply, lst)
     print("Multiplication is: ",Result)
